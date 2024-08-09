771094b68397f9bee64ab6ea81851a1ec860d030
Gabrielsantos552 <lilgabriel4244@gmail.com>
2024-08-09 14:41:17 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,6 +1,6 @@
 termo = int(input("Primeiro termo: "))
 quantidade = int(input("Quantidade de termos: "))
-razao = int(input("razâo: "))
+razao = int(input("razão: "))
 a = 0
 for x in range(termo, quantidade + 1, razao):
   a += x
