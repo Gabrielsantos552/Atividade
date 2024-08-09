8029e671f516cf152c076d70005611462184f896
Gabrielsantos552 <lilgabriel4244@gmail.com>
2024-08-09 14:39:41 -0300

    Update questão1.py

--- questão1.py
+++ questão1.py
@@ -1,8 +1,8 @@
-a = int(input("informe o primeiro número inteiro(a)"))
-b = int(input("informe o segundo número inteiro(a)"))
+a = int(input("informe o primeiro número inteiro(a): "))
+b = int(input("informe o segundo número inteiro(a): "))
 
 if a < b:
   soma = sum(range(a,b + 1))
   print(f"a soma dos número inteiros no intervalo [{a}, {b}] é:{soma}")
 else:
-  print("Erro:  O primeiro número deve ser menor que o segundo número")
+  print("Erro:  O primeiro número deve ser menor que o segundo número.")
